7e04deb291a42b9036dced94efca9fdc76565527
eslam-cmd <hdayaaslam34@email.com>
2025-08-10 10:18:05 +0300

    ename to empontact to force casing change

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import React from "react";
 import Header from "../../components/Ultimits/header";
 import Footer from "../../components/Ultimits/footer";
 
-import ContactPage from "../../components/contactpage/TempContact";
+import ContactPage from "../../components/contactpage/ContactPage";
 
 export default function ContactScreen({ toggleTheme, darkMode }) {
   return (
