f0d3e1cd158331feab68d8a65a56f7fc38181985
eslam-cmd <hdayaaslam34@email.com>
2025-08-10 09:49:26 +0300

    Refined UI design and theme updates

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import React from "react";
 import Header from "../../components/Ultimits/header";
 import Footer from "../../components/Ultimits/footer";
 
-import ContactPage from "../../components/contactpage/ContactPage";
+import ContactPage from "../../components/contactpage/Contactpage";
 
 export default function ContactScreen({ toggleTheme, darkMode }) {
   return (
